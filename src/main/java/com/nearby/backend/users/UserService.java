7d950fe43416cb1a4f5e51ad9898cb6365b5ac14
karandeep-kamboj <karandeep.kamboj@timesinternet.in>
2021-07-26 09:40:41 +0530

    / removed by lakshay

--- src/main/java/com/nearby/backend/users/UserService.java
+++ src/main/java/com/nearby/backend/users/UserService.java
@@ -1,8 +1,6 @@
 package com.nearby.backend.users;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
