1fb5170631995524be9614afe05f9e07827462b5
gulam waris <gulam.waris@timesinternet.in>
2021-07-30 09:19:07 +0530

    updated myOffers api

---
+++ src/main/java/com/nearby/backend/MyOffers/MyOfferRepository.java
@@ -0,0 +1,19 @@
+package com.nearby.backend.MyOffers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.nearby.backend.coupons.Coupon;
+
+@Repository
+public interface MyOfferRepository extends JpaRepository<MyOffers, String> {
+
+	@Query(value = "select * from myOffers" +  " where email = :email", nativeQuery = true)
+	MyOffers getOffersOfUser(@Param("email") String email);
+
+}
--- src/main/java/com/nearby/backend/MyOffers/MyOffersController.java
+++ src/main/java/com/nearby/backend/MyOffers/MyOffersController.java
@@ -4,7 +4,11 @@ package com.nearby.backend.MyOffers;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +26,24 @@ public class MyOffersController {
 		this.offerService = service;
 	}
 	
-	
-	@GetMapping(value = "/myOffers-purchased")
-	public List<Coupon> getAllOffers(MyOffers offers){
-		return offerService.allOffersPurchased(offers);
+	@GetMapping(value = "/test")
+	public String test() {
+		return "Success";
 	}
 	
-	@GetMapping(value = "/payment-success")
-	public Map<String, String> payment(Cart cart, boolean paid, MyOffers offers, String name, String phone, String address){
-		return offerService.paymentSuccess(cart,paid,offers,name,phone,address);
+	@GetMapping(value = "/findByTransaction/{email}/{date}")
+	public Coupon findByDate(@PathVariable(value = "email") String email, @PathVariable(value = "date") Long date) {
+		return offerService.findByTransactionDate(email, date);
 	}
+	
+	@GetMapping(value = "/myOffers-purchased/{email}")
+	public List<Coupon> getAllOffers(@PathVariable(value = "email") String email){
+		return offerService.allOffersPurchased(email);
+	}
+	
+//	@PutMapping(value = "/payment-success")
+//	public Map<String, String> payment(@Validated @RequestBody Cart cart, boolean paid, MyOffers offers, String name, String phone, String address){
+//		return offerService.paymentSuccess(cart,paid,offers,name,phone,address);
+//	}
 
 }
